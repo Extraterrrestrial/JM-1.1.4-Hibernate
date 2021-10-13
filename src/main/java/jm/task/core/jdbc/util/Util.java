39b42f22a478b55d1ed03cf794a8db95d66d2090
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-13 22:16:25 +0600

    Util commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 
 public class Util {
 //    реализуйте настройку соединения с БД
-    public static final String DRIVER="com.mysql.cj.jdbc.Driver";
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String HOST = "jdbc:mysql://localhost:3306/test?useSSL=false&allowMultiQueries=true&serverTimezone=UTC";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "root";
