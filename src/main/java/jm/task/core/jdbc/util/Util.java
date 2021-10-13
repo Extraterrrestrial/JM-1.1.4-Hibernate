cc29dea0dfeb4507228b560fc1d6747f402060f6
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-13 22:40:31 +0600

    Util commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,42 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
 //    реализуйте настройку соединения с БД
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String HOST = "jdbc:mysql://localhost:3306/test?useSSL=false&allowMultiQueries=true&serverTimezone=UTC";
-    private static final String LOGIN = "root";
-    private static final String PASSWORD = "root";
+    private static final String HOST = "jdbc:mysql://localhost:3306/test?serverTimezone=Europe/Moscow";
+    private static final String LOGIN = "root1";
+    private static final String PASSWORD = "1234";
+    private static SessionFactory sessionFactory = null;
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    public static SessionFactory getConnection() {
+
         try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            System.err.println("Не удалось загрузить класс драйвера!");
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable e) {
             e.printStackTrace();
         }
-        return connection;
+        return sessionFactory;
     }
 
-
-
+    public static void closeConnection() {
+        if (sessionFactory != null)
+            sessionFactory.close();
+    }
 }
