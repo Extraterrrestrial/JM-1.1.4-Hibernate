cc970c06cb0d6b388d634fa3d81dcdaffeea92c4
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-14 02:44:52 +0600

    changeы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -23,14 +23,11 @@
 //    }
 //}
 
-
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-
-
 
 public class Main {
     private static final UserService userService = new UserServiceImpl();
@@ -42,25 +39,12 @@ public class Main {
 
     public static void main(String[] args) {
         userService.createUsersTable();
-
         userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-
         userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-
         userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-
         userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-
         userService.getAllUsers();
-
-
-
-
-
         userService.cleanUsersTable();
-
         userService.dropUsersTable();
-
-
     }
 }
